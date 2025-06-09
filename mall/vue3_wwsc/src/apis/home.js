c2d58582e1f80fc835be791628bb113d009f0172
seventeen-1-7 <118060980+seventeen-1-7@users.noreply.github.com>
2025-06-09 17:11:42 +0800

    Update home.js

--- mall/vue3_wwsc/src/apis/home.js
+++ mall/vue3_wwsc/src/apis/home.js
@@ -1,13 +1,5 @@
 import http from "@/utils/http";
 
-/**
- * @description: 获取轮播图
- * @param distributionSite='1' 首页 '2'表示分类页面
- * @return {*}
- */
-export function getBannerAPI(distributionSite='1'){
-    return http.get('/home/banner',{params:{distributionSite}});
-}
 
 /**
  * @description: 获取新鲜好物
