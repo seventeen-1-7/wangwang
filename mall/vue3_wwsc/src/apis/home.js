548a04611138fe490621c6a4319626accfa03456
seventeen-1-7 <118060980+seventeen-1-7@users.noreply.github.com>
2025-06-09 17:13:46 +0800

    Update home.js

--- mall/vue3_wwsc/src/apis/home.js
+++ mall/vue3_wwsc/src/apis/home.js
@@ -1,7 +1,22 @@
 import http from "@/utils/http";
 
+/**
+ * @description: 获取轮播图
+ * @param distributionSite='1' 首页 '2'表示分类页面
+ * @return {*}
+ */
+export function getBannerAPI(distributionSite='1'){
+    return http.get('/home/banner',{params:{distributionSite}});
+}
 
-
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const getNewAPI = () => {
+    return http.get('/home/new')
+}
 
 /**
  * @description: 获取人气推荐
